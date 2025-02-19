7d1079b98b088da00bb339d1f2321e38dd1406db
andy.bezbozhny <andy.bezbozhny@gmail.com>
2025-02-19 17:51:01 +0300

    [*] #1 Импорт mission types (in progress...)

--- import.php
+++ import.php
@@ -1,9 +1,11 @@
 <?php
     error_reporting(E_ALL);
 
+    $begin  = time();
+
     $fname = basename(__FILE__);
 
-    $lock = __DIR__ . '/import.lock';
+    $lock = __FILE__ . '.lock';
     $fp = fopen($lock, 'w+');
 
     if (flock($fp, LOCK_EX | LOCK_NB)) {
@@ -14,18 +16,21 @@
         die;
     }
 
-    echo $fname . ' => has began' . PHP_EOL;
-
-    $begin  = time();
-
     # импорт
     require 'config.php';
+
+    if (SpaceBoteque::INSTANCE_SBQ == SpaceBoteque::$currentInstance) {
+        SpaceBoteque::log2file($fname . ' => has began');
+    }
+
+    goto missionType;
 
     # Загрузка в SpaceBoteque::$proxies адресов прокси-серверов
     if (SpaceBoteque::proxies()) {
         # ok
-    } else {
+    } else if (SpaceBoteque::$error) {
         # запись в error.log
+        SpaceBoteque::log2file(SpaceBoteque::$serror);
     }
 
     if (SpaceBoteque::$error) goto endOfScript;
@@ -43,13 +48,32 @@
 
     if (false === $missionTypes) {
         # запись в error.log
-    }
+        SpaceBoteque::log2file(SpaceBoteque::$error);
+    } else if (
+        array_key_exists('results', $missionTypes)
+        and
+        is_array($missionTypes['results'])
+    ) {
+        $missionTypes = $missionTypes['results'];
+        $missionTypes = array_combine(array_column($missionTypes, 'id'), array_column($missionTypes, 'name'));
+        ksort($missionTypes);
 
+        if ($diffMissionTypes = array_diff($missionTypes, SpaceBoteque::$allMissionTypes)) {
+            # todo Если есть какие-либо различия
+            # https://github.com/andybe29/sbq.server/issues/1
+        }
+    } else {
+        SpaceBoteque::$error = new stdClass;
+        SpaceBoteque::$error->message = 'Invalid value';
+        SpaceBoteque::$error->result  = $missionTypes;
+    }
     if (SpaceBoteque::$error) goto endOfScript;
 
     endOfScript:
 
-    echo $fname . ' => completed for ' . gmdate('H:i:s', time() - $begin) . PHP_EOL;
+    if (SpaceBoteque::INSTANCE_SBQ == SpaceBoteque::$currentInstance) {
+        SpaceBoteque::log2file($fname . ' => completed for ' . gmdate('H:i:s', time() - $begin));
+    }
 
     flock($fp, LOCK_UN);
     fclose($fp);
