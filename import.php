0945e9865d47537fd114c42d6d4acdca13fb5a25
andy.bezbozhny <andy.bezbozhny@gmail.com>
2025-02-24 22:02:29 +0300

    [*] import

--- import.php
+++ import.php
@@ -23,85 +23,65 @@
         SpaceBoteque::log2file($fname . ' => has began');
     }
 
-    goto launchStatuses;
+    $requestURL = implode([SpaceBoteque::getCurrentAPIURL(), '/launches/upcoming/']);
 
-    # Загрузка в SpaceBoteque::$proxies адресов прокси-серверов
-    if (SpaceBoteque::proxies()) {
-        # ok
-    } else if (SpaceBoteque::$error) {
-        # запись в error.log
-        SpaceBoteque::log2file(SpaceBoteque::$serror);
-    }
+    $query = [
+        'limit'    => 25,
+        'mode'     => 'detailed',
+        'offset'   => 0,
+        'ordering' => 'net'
+    ];
 
-    if (SpaceBoteque::$error) goto endOfScript;
+    $unknownMissionTypes = [];
 
-    # сортировка массива по возрастанию id
-    function cmp($a, $b) {
-        return ($a['id'] == $b['id']) ? 0 : (($a['id'] < $b['id']) ? -1 : 1);
-    }
+    $agency         = new Agency($sql);
 
-    goto endOfScript;
+    $launchStatus   = new LaunchStatus($sql);
+    $launchStatuses = ($launchStatuses = $launchStatus->all()) ? array_column($launchStatuses, 'id') : [];
 
-    launchStatuses:
-    # импорт значений статусов пусков
+    do {
+        $url = $requestURL . '?' . http_build_query($query);
 
-    $launchStatus = new LaunchStatus($sql);
-    $launchStatuses = $launchStatus->import();
+        echo $url . PHP_EOL;
 
-    if (false === $launchStatuses) {
-        SpaceBoteque::log2file(SpaceBoteque::$error);
-    } else {
+        $response = SpaceBoteque::requestURL($url);
 
-        uasort($launchStatuses, 'cmp');
+        if (empty(SpaceBoteque::$error)) {
+            foreach ($response['results'] as $currentLaunchNode) {
+                $currentMissionNode = $currentLaunchNode['mission'];
 
-        foreach ($launchStatuses as $launchStatusValue) {
-            if ($launchStatus->replace($launchStatusValue)) {
-                # ok
-            } else if (empty($sql->err)) {
-                SpaceBoteque::log2file(SpaceBoteque::$error);
+                if (in_array($currentMissionNode['type'], MissionType::MISSION_TYPES)) {
+                    # to process
+                    $json = json_encode($currentLaunchNode, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE);
+                    $flog = SpaceBoteque::$instancePath . '/tmp/' . $currentLaunchNode['id'] . '.json';
+                    error_log($json, 0, $flog);
+
+
+                } else if (!in_array($currentMissionNode['type'], MissionType::MISSION_TYPES_ALL)) {
+                    # unknown Mission Type
+                    $unknownMissionTypes[] = $currentMissionNode['type'];
+                }
             }
         }
 
-    }
-
-    goto endOfScript;
-
-    missionTypes:
-    # импорт значений MISSION_TYPE
-
-    $missionTypesURL  = implode([SpaceBoteque::llAPI(), SpaceBoteque::MISSION_TYPES_LLAPI_URI]);
-    $missionTypesURL .= '?' . http_build_query(SpaceBoteque::LL_API_QUERY);
-
-    $missionTypes = SpaceBoteque::requestURL($missionTypesURL);
-
-    if (false === $missionTypes) {
-        # запись в error.log
-        SpaceBoteque::log2file(SpaceBoteque::$error);
-    } else if (array_key_exists('results', $missionTypes) and is_array($missionTypes['results'])) {
-
-        $missionTypes = $missionTypes['results'];
-        $missionTypes = array_combine(array_column($missionTypes, 'id'), array_column($missionTypes, 'name'));
-
-        ksort($missionTypes);
-
-        if ($diffMissionTypes = array_diff($missionTypes, SpaceBoteque::$allMissionTypes)) {
-            # todo Если есть какие-либо различия
-            # https://github.com/andybe29/sbq.server/issues/1
+        if (SpaceBoteque::$error) {
+            echo json_encode(SpaceBoteque::$error, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE) . PHP_EOL;
         }
 
-    } else {
-        SpaceBoteque::$error = new stdClass;
-        SpaceBoteque::$error->message = 'Invalid value';
-        SpaceBoteque::$error->result  = $missionTypes;
+        $query['offset'] += $query['limit'];
 
-        SpaceBoteque::log2file(SpaceBoteque::$error);
+    } while (empty(SpaceBoteque::$error) and !empty($response['next']));
+
+    if ($unknownMissionTypes) {
+        $unknownMissionTypes = array_unique($unknownMissionTypes);
+        echo 'Unknown Mission Types: ' . json_encode($unknownMissionTypes, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE) . PHP_EOL;
     }
-    if (SpaceBoteque::$error) goto endOfScript;
 
     endOfScript:
 
     if (SpaceBoteque::INSTANCE_SBQ == SpaceBoteque::$currentInstance) {
         SpaceBoteque::log2file($fname . ' => completed for ' . gmdate('H:i:s', time() - $begin));
+        SpaceBoteque::log2file(SpaceBoteque::$requestedURLs . ' completed');
     }
 
     flock($fp, LOCK_UN);
